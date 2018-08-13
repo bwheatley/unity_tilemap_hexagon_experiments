6668ff0ba8b36e61320e903c7652d9ce6babf3e2
Will Wheatley <bill@williamwheatley.com>
2018-08-13 17:34:42 -0400

    First commit

---
+++ Assets/_tilemaps/_scripts/core/Hex.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// The hex class defines the grid position, world space position, size, neighbors, etc of a hex tile.
+/// However it does NOT interact with unity directly in anyway.
+/// </summary>
+public class Hex {
+
+    public Hex(int q, int r)
+    {
+        this.Q = q;
+        this.R = r;
+        this.S = -(q + r);
+    }
+
+
+    // Q + R + S = 0
+    // S = -( Q + R )
+
+    public readonly int Q;  //Column
+    public readonly int R;  //Row
+    public readonly int S;  //Sum
+
+    static private readonly float WIDTH_MULTIPLIER = Mathf.Sqrt(3) / 2;
+
+    /// <summary>
+    /// This returns the world-space position of this hex
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 Position() {
+
+        float radius = 1f;
+        float height = radius * 2;  //AKA Diameter
+        float width = WIDTH_MULTIPLIER * height;
+
+        float horiz = width;
+        float vert = height * 0.75f;  // 3/4
+        
+
+
+        return new Vector3(horiz * (this.Q + this.R/2f)  ,
+            vert * this.R,
+            0);
+    }
+
+
+}
---
+++ Assets/_tilemaps/_scripts/core/HexMap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour {
+
+    public GameObject HexPrefab;
+    public Material[] HexMaterials;
+
+
+	// Use this for initialization
+	void Start () {
+		GenerateMap();
+	}
+
+    public void GenerateMap() {
+
+        for (int column = 0; column < 10; column++) {
+            for (int row = 0; row < 10; row++) {
+         
+                Hex h = new Hex(column, row);
+                
+                //Instantiate a hex
+                GameObject hexGO = (GameObject) Instantiate(HexPrefab, h.Position(), Quaternion.identity, this.transform);
+
+                MeshRenderer mr = hexGO.GetComponentInChildren<MeshRenderer>();
+                mr.material = HexMaterials[Random.Range(0, HexMaterials.Length)];
+
+            }
+        }
+
+        //If our map doesn't move we can do this
+        //StaticBatchingUtility.Combine(this.gameObject);
+
+
+    }
+
+}
